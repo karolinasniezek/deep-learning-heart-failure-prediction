1b65c3d575385961c373ec004cdae98382efd0d8
Karolina Śnieżek <karolina.sniezek@xebia.com>
2026-06-21 19:15:05 +0200

    add test, train data and scaler

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("../data/heart.csv")
 
@@ -13,4 +16,17 @@ df = pd.get_dummies(
     ]
 )
 
-print(df.head())
+X = df.drop("HeartDisease", axis=1)
+y = df["HeartDisease"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
